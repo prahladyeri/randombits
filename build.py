9034b02fde89abe71d3a470b00df919b6f728b0b
Prahlad Yeri <prahladyeri@yahoo.com>
2021-10-10 08:30:27 +0530

    build: random bits

---
+++ build.py
@@ -0,0 +1,27 @@
+import uuid
+
+readme = """```bash
+
+This is an auto-generated github repository that just pushes some random bits to keep your github activity going. Useful when you're on a vacation or just temporarily lost inspiration to code!
+
+%code1%
+%code2%
+%code3%
+
+Running:
+
+python build.py
+git add .
+git commit -m "build: random bits"
+git push
+```
+"""
+def main():
+    rm = readme
+    rm = rm.replace('%code1%', str(uuid.uuid4()))
+    rm = rm.replace('%code2%', str(uuid.uuid4()))
+    rm = rm.replace('%code3%', str(uuid.uuid4()))
+    open('readme.md', 'w').write(rm)
+    
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,15 @@
+```bash
+
+This is an auto-generated github repository that just pushes some random bits to keep your github activity going. Useful when you're on a vacation or just temporarily lost inspiration to code!
+
+9ac6c0af-3107-44b9-a1be-2cce9963376c
+e9b8e2cd-dd0c-45be-a327-9b984ebc57d5
+5829904c-2fdf-4b4b-98ec-7496473db8d8
+
+Running:
+
+python build.py
+git add .
+git commit -m "build: random bits"
+git push
+```
